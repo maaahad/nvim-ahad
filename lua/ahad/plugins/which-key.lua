f0188605dfed8a4bdaac26f7b805cec08030c897
Muhammed Ahad <maaahad@gmail.com>
2024-07-13 23:27:57 +0200

    which key kemaps setup based on updated plugin

--- lua/ahad/plugins/which-key.lua
+++ lua/ahad/plugins/which-key.lua
@@ -9,33 +9,21 @@ return {
     plugins = { spelling = true },
     defaults = {
       mode = { "n", "v" },
-      -- ["g"] = { name = "+goto" },
-      -- ["gs"] = { name = "+surround" },
-      -- ["z"] = { name = "+fold" },
-      -- ["]"] = { name = "+next" },
-      -- ["["] = { name = "+prev" },
-      -- ["<leader>b"] = { name = "+buffer" },
-      -- ["<leader>c"] = { name = "+code" },
-      -- ["<leader>gh"] = { name = "+hunks", ["_"] = "which_key_ignore" },
-      -- ["<leader>s"] = { name = "+search" },
-      -- ["<leader>u"] = { name = "+ui" },
-      -- ["<leader>x"] = { name = "+diagnostics/quickfix" },
-
-      -- TODO: my settings
-      ["<leader>p"] = { name = "+[ p ]ersistence" },
-      ["<leader>f"] = { name = "+[ f ]ile / [ f ]ind" },
-      ["<leader>g"] = { name = "+[ g ]it" },
-      ["<leader><tab>"] = { name = "+[ tab ]s" },
-      ["<leader>w"] = { name = "+[ w ]indows" },
-      ["<leader>n"] = { name = "+[ n ]ew of or set [ n ]o to" },
-      ["<leader>e"] = { name = "+[ e ]xplore" },
-      ["<leader>s"] = { name = "+[ s ]ession / [ s ]earch" },
-      ["<leader>q"] = { name = "+[ q ]uit" },
+      { "<leader>p" , group = "+[ p ]ersistence" },
+      { "<leader>f" , group = "+[ f ]ile / [ f ]ind" },
+      { "<leader>g" , group = "+[ g ]it" },
+      { "<leader><tab>" , group = "+[ tab ]s" },
+      { "<leader>w" , group = "+[ w ]indows" },
+      { "<leader>n" , group = "+[ n ]ew of or set [ n ]o to" },
+      { "<leader>e" , group = "+[ e ]xplore" },
+      { "<leader>s" , group = "+[ s ]ession / [ s ]earch" },
+      { "<leader>q" , group = "+[ q ]uit" },
     },
   },
   config = function(_, opts)
     local wk = require("which-key")
     wk.setup(opts)
-    wk.register(opts.defaults)
+    -- adding keymaps
+    wk.add(opts.defaults)
   end,
 }
