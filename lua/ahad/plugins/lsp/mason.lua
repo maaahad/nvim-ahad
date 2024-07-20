4e91156cc8f0364591120aa948f0763694496270
Muhammed Ahad <maaahad@gmail.com>
2024-07-20 13:03:24 +0200

    mason-tool-installer setup

--- lua/ahad/plugins/lsp/mason.lua
+++ lua/ahad/plugins/lsp/mason.lua
@@ -1,37 +1,48 @@
 return {
-  "williamboman/mason.nvim", 
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim", 
-  }, 
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        }, 
-      }, 
-    })
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      -- list of server for mason to install
-      ensure_installed = {
-        "tsserver", 
-        "html", 
-        "cssls", 
-        "tailwindcss", 
-        "svelte", 
-        "lua_ls", 
-        "graphql", 
-        "emmet_ls", 
-        "prismals", 
-        "pyright", 
-      }, 
-    })
-  end
+		mason_lspconfig.setup({
+			-- list of server for mason to install
+			ensure_installed = {
+				"tsserver",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"lua_ls",
+				"graphql",
+				"emmet_ls",
+				"prismals",
+				"pyright",
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- pytohn formatter
+			},
+		})
+	end,
 }
