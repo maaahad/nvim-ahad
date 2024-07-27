bef490c7926466aecc4e7e9d724813ca7327692b
Muhammed Ahad <maaahad@gmail.com>
2024-07-28 00:32:48 +0200

    disabled catppuccin theme

--- lua/ahad/plugins/catppuccin.lua
+++ lua/ahad/plugins/catppuccin.lua
@@ -1,12 +1,11 @@
 return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  -- enabled / disabled
-  -- enabled = false,
-  priority = 1000,
-  enable = false, 
-  config = function()
-    vim.cmd.colorscheme("catppuccin-mocha")
-  end,
+	"catppuccin/nvim",
+	lazy = false,
+	name = "catppuccin",
+	-- enabled / disabled
+	enabled = false,
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("catppuccin-mocha")
+	end,
 }
