2f3f00e3ea0f02d433be9808fef54b653d7436a0
Muhammed Ahad <maaahad@gmail.com>
2024-08-03 15:40:55 +0200

    starrify theme in alpha

--- lua/ahad/plugins/alpha.lua
+++ lua/ahad/plugins/alpha.lua
@@ -2,39 +2,45 @@ return {
 	"goolord/alpha-nvim",
 	event = "VimEnter",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		position = "center",
+	},
 	config = function()
 		local alpha = require("alpha")
-
-		local dashboard = require("alpha.themes.dashboard")
-
-		-- set header
-		dashboard.section.header.val = [[
-   ________  ________  ________  ________      ________  ________  ________   _______ 
-  ╱        ╲╱        ╲╱  ╱  ╱  ╲╱        ╲    ╱        ╲╱    ╱   ╲╱        ╲_╱       ╲
- ╱         ╱         ╱         ╱         ╱   ╱         ╱         ╱         ╱         ╱
-╱         ╱        _╱         ╱         ╱   ╱         ╱         ╱         ╱         ╱ 
-╲___╱____╱╲____╱___╱╲________╱╲___╱____╱    ╲___╱____╱╲___╱____╱╲___╱____╱╲________╱  
-    ]]
-
-		--set menu
-		-- more config ref: https://github.com/LunarVim/LunarVim/issues/2801
-		dashboard.section.buttons.val = {
-			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Neotree toggle<CR>"),
-			dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button(
-				"SPC sl",
-				"󰁯  > Restore Session For Current Directory",
-				"require('persistence').load()"
-			),
-			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
-			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-		}
-
-		-- set config to alpha
-		alpha.setup(dashboard.opts)
-		-- disable folding on alpha buffer
-		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+		local startify = require("alpha.themes.startify")
+		startify.section.header.val = [[
+	   ________  ________  ________  ________      ________  ________  ________   _______
+	  ╱        ╲╱        ╲╱  ╱  ╱  ╲╱        ╲    ╱        ╲╱    ╱   ╲╱        ╲_╱       ╲
+	 ╱         ╱         ╱         ╱         ╱   ╱         ╱         ╱         ╱         ╱
+	╱         ╱        _╱         ╱         ╱   ╱         ╱         ╱         ╱         ╱
+	╲___╱____╱╲____╱___╱╲________╱╲___╱____╱    ╲___╱____╱╲___╱____╱╲___╱____╱╲________╱
+	    ]]
+		alpha.setup(startify.config)
 	end,
+
+	-- config = function()
+	-- 	local alpha = require("alpha")
+	-- 	local dashboard = require("alpha.themes.dashboard")
+	-- 	dashboard.section.header.val = [[
+	--    ________  ________  ________  ________      ________  ________  ________   _______
+	--   ╱        ╲╱        ╲╱  ╱  ╱  ╲╱        ╲    ╱        ╲╱    ╱   ╲╱        ╲_╱       ╲
+	--  ╱         ╱         ╱         ╱         ╱   ╱         ╱         ╱         ╱         ╱
+	-- ╱         ╱        _╱         ╱         ╱   ╱         ╱         ╱         ╱         ╱
+	-- ╲___╱____╱╲____╱___╱╲________╱╲___╱____╱    ╲___╱____╱╲___╱____╱╲___╱____╱╲________╱
+	--     ]]
+	-- 	dashboard.section.buttons.val = {
+	-- 		dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	-- 		dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+	-- 	}
+	-- 	local handle = io.popen("fortune")
+	-- 	local fortune = handle:read("*a")
+	-- 	handle:close()
+	-- 	dashboard.section.footer.val = fortune
+	--
+	-- 	dashboard.config.opts.noautocmd = true
+	--
+	-- 	vim.cmd([[autocmd User AlphaReady echo 'ready']])
+	--
+	-- 	alpha.setup(dashboard.config)
+	-- end,
 }
