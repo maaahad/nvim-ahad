caa9bbf4c37324eb3487108fc9b1b492e1a98312
Muhammed Ahad <maaahad@gmail.com>
2024-08-11 00:07:10 +0200

    luasnip format

--- lua/ahad/plugins/luasnip.lua
+++ lua/ahad/plugins/luasnip.lua
@@ -4,16 +4,16 @@ return {
 	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 	-- install jsregexp (optional!).
 	build = "make install_jsregexp",
-	-- config = function()
-	-- 	local ls = require("luasnip")
-	--
-	-- 	vim.keymap.set({ "i" }, "<C-k>", function()
-	-- 		ls.expand()
-	-- 	end, { silent = true })
-	--
-	-- 	-- VS code like snippets
-	-- 	require("luasnip.loaders.from_vscode").lazy_load()
-	--
-	-- 	ls.setup({})
-	-- end,
+	config = function()
+		local ls = require("luasnip")
+
+		vim.keymap.set({ "i" }, "<C-k>", function()
+			ls.expand()
+		end, { silent = true })
+
+		-- VS code like snippets
+		require("luasnip.loaders.from_vscode").lazy_load()
+
+		ls.setup({})
+	end,
 }
